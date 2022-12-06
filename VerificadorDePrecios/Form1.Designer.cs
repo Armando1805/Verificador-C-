0fc303409648c43ec7e6fa4f38cb92fc2da53b4f
Armando1805 <armando1805@hotmail.com>
2022-12-06 16:21:18 -0700

    Proyecto C#

---
+++ VerificadorDePrecios/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace VerificadorDePrecios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(484, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lector de Productos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VerificadorDePrecios.Properties.Resources.barcode_scan;
+            this.pictureBox1.Location = new System.Drawing.Point(354, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(313, 590);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(533, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pase el producto por el lector";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 42);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 42);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Precio";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1207, 665);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ VerificadorDePrecios/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerificadorDePrecios
+{
+    public partial class Form1 : Form
+    {
+        string Codigo = "";
+        string Ruta = "C:\\Users\\Armando\\Desktop\\verificadordeprecioscsharp\\VerificadorDePrecios\\Resources";
+        string [,]Productos =
+        {
+            {"1",   "Call Of Duty", "59.20", "callofduty.jpg"},
+            {"2",   "Crash Bandicoot", "60.70", "crash.jpg"},
+            {"3",   "Destiny", "49.44", "destiny.jpg"},
+            {"4",   "Dragon Ball Kakarot", "69.70", "Dragonball.jpg"},
+            {"5",   "Final Fantasy 14", "57.40", "ff14.jpg"},
+            {"6",   "God Of War Ragnarok", "79.00", "GODR.jpg"},
+            {"7",   "Resident Evil 3", "59.00", "re3.jpg"},
+            {"8",   "Resident Evil 8", "69.00", "residen8.jpg"},
+            {"9",   "Tekken 7", "59.00", "tekken.jpg"},
+            {"10",  "Uncharted 4", "59.56", "uncharted.jpg"}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Location = new Point(this.Width / 2 - label1.Width / 2, 50);
+            
+            label2.Location = new Point(this.Width / 2 - label2.Width / 2, this.Height - label2.Height - 50);
+            label3.Visible = false; 
+            label4.Visible = false;
+            Centrar_Imagen();
+        }
+
+        private void Centrar_Imagen()
+        {
+            pictureBox1.Location = new Point(this.Width / 2 - pictureBox1.Width / 2, this.Height / 2 - pictureBox1.Height / 2);
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if (e.KeyChar != 13)
+            {
+                Codigo += e.KeyChar;
+                
+
+            }
+            else
+            {
+                timer1.Enabled = false;
+                timer1.Start();
+                timer1.Enabled = true;
+             
+                //MessageBox.Show("Código " + Codigo);
+                for (int i = 0; i < 10; i++)
+                {
+                    if (Codigo == Productos[i, 0])
+                    {
+
+                        label2.Visible = false;
+                        label3.Text = Productos[i, 1];
+                        label3.Visible = true;
+                        label4.Text = "$ " + Productos[i, 2];
+                        label4.Visible = true;
+                        label3.Location = new Point(this.Width / 2 - label3.Width / 2, (this.Height - label3.Height*2)-50);
+                        label4.Location = new Point(this.Width / 2 - label4.Width / 2, (this.Height - label4.Height)-50);
+
+                        
+                        pictureBox1.Image = Image.FromFile(Ruta + @"\" + Productos[i, 3]);
+                        pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                        Centrar_Imagen();
+
+                    }
+                }
+                Codigo = "";
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(Ruta + @"\barcode-scan.gif");
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            Centrar_Imagen();
+            label2.Visible = true;
+            label3.Visible = false;
+            label4.Visible = false;
+            timer1.Stop();
+        }
+    }
+}
